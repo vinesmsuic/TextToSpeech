7c1402aa822a85737b075d0e802d49fb804f16a6
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2018-09-04 18:09:24 +0800

    Update

--- app/src/main/java/com/example/max/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/max/texttospeech/MainActivity.java
@@ -1,32 +1,27 @@
 package com.example.max.texttospeech;
 
 import android.os.Build;
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
 
-    Button btnSubmit;
+    Button btnTalk;
     TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTalk = findViewById(R.id.btnTalk);
     }
 
     @Override
